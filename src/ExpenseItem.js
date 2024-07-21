9baf57d4890aa9644638d5b01ee7f7ebeb17762a
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-07-21 19:44:07 +0530

    refactor: Update ExpenseItem and Expenses components to use Card component as wrapper

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,16 +1,17 @@
+import Card from './Card.js';
 import ExpenseDate from './ExpenseDate.js';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
     return (
         <div>
-            <div className="expense-item">
+            <Card className="expense-item">
                 <ExpenseDate date={props.date} />
                 <div>
                     <h2 className="expense-item__description">{props.title}</h2>
                 </div>
                 <div className="expense-item__price">$ {props.amount}</div>
-            </div>
+            </Card>
         </div>
     )
 }

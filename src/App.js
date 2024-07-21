1c93ef56b5156253f664aa4e0bf46ec839a1acb0
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-07-21 22:12:47 +0530

    refactor: Update styles

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div>
-      <h1>Expense Management System!</h1>
+      <h1><center>Expense Management Application!</center></h1>
       <NewExpenseForm />
       <Expenses expenses={expenses} />
     </div>
